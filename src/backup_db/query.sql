0bda3c655e77ba6de7122d41d77e4f944aa784b4
Trotta Rosario <appero.app@gmail.com>
2025-09-15 07:43:13 +0200

    import backup db complete

--- src/backup_db/query.sql
+++ src/backup_db/query.sql
@@ -3,13 +3,13 @@ SELECT *
 FROM public.odds o
 INNER JOIN public.match m ON o.id_match = m.id_match_fk
 WHERE m.date_match >= '2025-09-13 00:00:00'
-  AND m.date_match < '2025-09-14 00:00:00'
-ORDER BY m.id_match_fk ASC;
+  AND m.date_match < '2025-09-15 00:00:00'
+ORDER BY m.current_league ASC;
 
 --Query statistics join match by date
 SELECT *
 FROM public.statistics s
 INNER JOIN public.match m ON s.id_match = m.id_match_fk
-WHERE m.date_match >= '2025-09-14 00:00:00'
+WHERE m.date_match >= '2025-09-13 00:00:00'
   AND m.date_match < '2025-09-15 00:00:00'
-ORDER BY m.id_match_fk ASC;
+ORDER BY m.current_league ASC;
