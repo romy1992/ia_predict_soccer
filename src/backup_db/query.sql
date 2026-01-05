956e96392c39fc2197929d067c093e8b4e5af404
Trotta Rosario <appero.app@gmail.com>
2026-01-05 07:21:25 +0100

    minor

--- src/backup_db/query.sql
+++ src/backup_db/query.sql
@@ -2,16 +2,16 @@
 SELECT *
 FROM public.odds o
 INNER JOIN public.match m ON o.id_match = m.id_match_fk
-WHERE m.date_match >= '2025-12-05 00:00:00'
-  AND m.date_match < '2025-12-09 00:00:00'
+WHERE m.date_match >= '2026-01-04 00:00:00'
+AND m.date_match < '2026-01-05 00:00:00'
 ORDER BY m.current_league ASC;
 
 --Query statistics join match by date
 SELECT *
 FROM public.statistics s
 INNER JOIN public.match m ON s.id_match = m.id_match_fk
-WHERE m.date_match >= '2025-09-13 00:00:00'
-  AND m.date_match < '2025-09-15 00:00:00'
+WHERE m.date_match >= '2026-01-04 00:00:00'
+AND m.date_match < '2026-01-05 00:00:00'
 ORDER BY m.current_league ASC;
 
 --Query statistics and odds join match by date
@@ -19,8 +19,8 @@ SELECT *
 FROM public.odds o
 INNER JOIN public.match m ON o.id_match = m.id_match_fk
 INNER JOIN public.statistics s ON s.id_match = m.id_match_fk
-WHERE m.date_match >= '2025-09-19 00:00:00'
-  AND m.date_match < '2025-09-20 00:00:00'
+WHERE m.date_match >= '2026-01-04 00:00:00'
+AND m.date_match < '2026-01-05 00:00:00'
 ORDER BY m.current_league ASC;
 
 -- Check per controllare se alcuni json delle statistics hanno dei valori particolari, in questo caso '^form'
